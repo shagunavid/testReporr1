8c0e0c647e9056db7739193fc5e72fdebf0b4bcc
shagunavid <shagun@avidsecure.io>
2018-12-07 18:28:54 +0530

    test shagun

--- demoCT.tf
+++ demoCT.tf
@@ -215,6 +215,7 @@ resource "aws_cloudtrail"  "example" {
 }
 
 
+
 #Ensure a log metric filter and alarm exist for Management Console sign-in without MFA
 resource "aws_cloudwatch_log_metric_filter" "MFAUsed" {
   name           = "console-without-mfa"
