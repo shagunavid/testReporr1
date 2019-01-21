adcbb548521a361314b5b38a52065042bc440e0b
shagunavid <shagun@avidsecure.io>
2019-01-21 17:00:23 +0530

    shagun

--- demoCT.tf
+++ demoCT.tf
@@ -185,7 +185,7 @@ resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length        = 16
   require_lowercase_characters   = false
   require_numbers                = false
-  require_uppercase_characters   = true
+  require_uppercase_characters   = false
   require_symbols                = true
   allow_users_to_change_password = true
 }
